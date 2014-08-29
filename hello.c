f681d7e113e81ce945f8ab131ecd7b4cb066cacf
Vahid_Sheigani <v.sheigani@yahoo.com>
2014-08-29 13:15:44 +0430

    First day of Life

---
+++ hello.c
@@ -0,0 +1,14 @@
+// This project is an experimental project performed by Mr.Vahid Sheigani
+// Dated In 2014.08.29
+
+
+#include <stdio.h>
+
+
+int main ()
+{
+	printf("This program says Hello to to you:D\n\n");
+
+	printf("Hello World\n");
+	return 0;
+}
